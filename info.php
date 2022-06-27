8281eb1c9f824e8a82ed45b5833c161edbbf0f1d
crashmyname <71687757+crashmyname@users.noreply.github.com>
2022-06-27 10:17:54 +0700

    Program Lier

--- info.php
+++ info.php
@@ -4,9 +4,16 @@ include('./layout/header.php');
 ?>
 
 <div class="container-fluid">
-    <h1> Klinik Jumiati adalah Klinik ibu dan anak yang bertempat di Bitung Tangerang-Banten.</h1>
+    <div class="card-body bg-white rounded">
+    <h2 align="center"> Klinik Jumiati adalah Klinik ibu dan anak yang bertempat di Bitung Tangerang-Banten.</h2><br>
+    <p><b>Sejarah</b> Klinik Jumiati berdiri pada tahun 1996 yang berlokasi di Jl. Pajajaran No.14 No 62, RT.002/RW.005, Gandasari, Kec. Jatiuwung, Kota Tangerang, Banten 15137. Klinik Jumiati bergerak dibidang layanan kesehatan masyarakat yang dibangun dengan harapan mampu melayani masyarakat yang membutuhkan pertolongan. Klinik Jumiati didirikan oleh seorang Dokter yang telah terdaftar di Dinas Kesehatan Kota Tangerang. Klinik Jumiati membuka layanan Kesehatan Ibu dan Anak, Dokter Kandungan, Persalinan dan Keluarga Berencana.</p>
+    <p><b>Visi</b> "Menjadikan Klinik Jumiati sebagai Klinik pilihan masyarakat, dengan Pelayanan Kesehatan yang berkualitas, Harga Terjangkau, Pelayanan yang cepat dan tepat, serta mengutamakan keselamatan Pasien".</p>
+    <p><b> Misi </b> <br>
+       1)	Memberikan pelayanan kesehatan terbaik dan dapat dipertanggung jawabkan. <br>
+       2)	Menyediakan sarana dan prasarana bermutu baik & terpercaya. <br>
+       3)	Memberikan pelayanan dengan biaya terjangkau cepat, tepat dan professional.</p>
 </div>
-
+</div>
 </div>
 <?php
 
